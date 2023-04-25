0981ceb5ad4707aa712728680e9606d246064206
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-04-24 17:05:00 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main a programme that prints a line with puts functions
+ *
+ * return: 0 (succes)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
