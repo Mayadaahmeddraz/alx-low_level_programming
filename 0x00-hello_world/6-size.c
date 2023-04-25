1f0cb6a9ceb28ae6f9e36376634b1a4b13b0e964
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-04-24 19:26:01 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - A programme to print the size of the characters
+ *
+ * Return: 0(success)
+ */
+
+
+int main(void)
+{
+	printf("Size of a char %d bite(s)\n", sizeof(char));
+	printf("Size of an int %d bite(s)\n", sizeof(int));
+	printf("Size of a long int %d bite(s)\n", sizeof(long int));
+	printf("Size of along long int %d bite(s)\n", sizeof(long long int));
+	printf("Size of a float %d bite(s)\n", sizeof(float));
+	return (0);
+}
