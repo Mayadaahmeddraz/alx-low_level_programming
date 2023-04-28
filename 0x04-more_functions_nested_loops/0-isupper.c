5dd9becfe57052957fd25acee0e96cc4b9f6103d
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-04-28 13:35:55 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ *_isupper - function to print alphapets with uppercase
+ *
+ *Return: 0 always
+ */
+
+int _isupper(int c);
+
+{
+	char ch = 'A';
+
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+	ch++
+	}
+	return (0);
+}
