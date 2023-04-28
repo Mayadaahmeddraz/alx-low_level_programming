0d9a46ea4c483683b2382112221b251f4f63153e
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-04-28 14:15:18 -0700

    all

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit function to print last digit
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int);
+{
+	int 
+		_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+}
