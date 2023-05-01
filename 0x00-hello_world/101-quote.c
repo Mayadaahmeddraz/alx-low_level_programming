f50b540b5a5acc9299ea26538175982072003083
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-05-01 06:59:45 -0700

    34

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <uni std.h>
+
+/**
+ *main - Entry point
+ *
+ *Discription: print write function write (int, const void, buf size of -t num)
+ *Return: 1 (not success)
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, msg, 59);
+	return (1);
+}
