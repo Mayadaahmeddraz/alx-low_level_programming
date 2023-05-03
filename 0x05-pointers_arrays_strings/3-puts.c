80598ae05823d62e1655eeec39ac3225fba0279b
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-05-02 18:03:32 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts -  a function that prints a string
+ *
+ *@str: input parameter
+ *
+ *Return: null
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar ('\n');
