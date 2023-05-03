a0fcfb55a93275c0dc1aa8b2205cf080553db990
mayadaahmeddraz <mayadadraz1@gmail.com>
2023-05-02 20:10:54 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,9 +5,21 @@
  *
  *@s: input parameter
  *
- *Return:
+ *Return:null
  */
 
 void rev_string(char *s)
 {
+	int m, i;
+	char temp;
+
+	/*find string length without null char*/
+	for (m = 0; s[m] != '\0'; ++1)
+		;
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < m / 2; i++)
+
+	temp = s[i];
+	s[i] = s[m - 1 - i]; /*-1 because the array starts from 0*/
+	s[m - 1 - i] = temp;
 }
